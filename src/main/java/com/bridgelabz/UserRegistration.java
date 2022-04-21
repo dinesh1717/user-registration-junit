612b8076768f64be700936f139bbffef7e640c37
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-21 23:26:09 +0530

    added validate first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -17,70 +17,8 @@ public class UserRegistration {
         matcher = pattern.matcher(name);
         return matcher.matches();
     }
-
-   // methodlastName to validation of the name value
-
-
-    public boolean lastName(String name) {
-        String regex = "^[A-Z]{1}[a-z]{2,}";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-
-
-
-
-    public boolean emailValidation(String name) {
-        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z][2,3]){0,1}$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-
-   // method to validation of mobile number data
-
-
-    public boolean numberValidation(String number) {
-        String regex = "^[0-9]{2}\\s[0-9]{10}";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(number);
-        return matcher.matches();
-    }
-
-    // methodlogin to validation of login value should have minimum eight value data
-
-
-    public boolean loginValidation(String name) {
-        String regex = "[A-Za-z0-9]{8,}";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-
-    //create method to validation one or greater digit value data
-    public boolean loginValidationOneUpperCase(String name){
-        String regex = "(?=.*[A-Z]){1}(?=.*[0-9])(?=.*[a-z]).{8,}$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-    //create method to validation one or greater digit value data
-    public boolean loginValidationOneNumber(String name){
-        String regex = "(?=.*[A-Z]){1}(?=.*[0-9])(?=.*[a-z]).{8,}$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-
-    //create methofd to validation one special value data
-    public boolean loginValidationOneSpecialCharacter(String name){
-        String regex = "^(?=.*[A-Z]){1}(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%!]).{8,}$";
-        pattern = Pattern.compile(regex);
-        matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
 }
+
 
 
 
