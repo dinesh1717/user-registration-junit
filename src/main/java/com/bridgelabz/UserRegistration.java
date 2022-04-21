52027408d6ebe0265fb1ca01f1c2a9ff335841a1
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-21 23:43:43 +0530

    added:mobile number validation

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -35,6 +35,15 @@ public class UserRegistration {
         matcher = pattern.matcher(name);
         return matcher.matches();
     }
+
+    // method to validation of mobile number data
+
+    public boolean numberValidation(String number){
+        String regex = "^[0-9]{2}\\s[0-9]{10}";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(number);
+        return matcher.matches();
+    }
 }
 
 
